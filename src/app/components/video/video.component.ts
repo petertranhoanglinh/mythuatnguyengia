6a3373d7939a81d0a6e1525ecaf93dbf40ea05a7
Webixx-Store <contact.webixx@gmail.com>
2025-03-06 15:26:56 +0700

    A

--- src/app/components/video/video.component.ts
+++ src/app/components/video/video.component.ts
@@ -1,76 +1,35 @@
-// video.component.ts
-import { Component, OnInit } from '@angular/core';
-import { environment } from 'src/environments/environment';
-
-interface Video {
-  id: string;
-  title: string;
-  description: string;
-  thumbnail: string;
-}
+// video-list.component.ts
+import { Component, Sanitizer } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-video',
   templateUrl: './video.component.html',
   styleUrls: ['./video.component.css']
 })
-export class VideoComponent implements OnInit {
-  videos: Video[] = [
-    {
-      id:   '../../../assets/video/6349499914477.mp4',
-      title: 'Video Tranh Tường 1',
-      description: 'Mô tả ngắn về video 1',
-      thumbnail: '../../../assets/hinh/z6348903745611_dbdefa74a73aac356fd4ec112bcc285b.jpg'
-    },
-    {
-      id:   '../../../assets/video/6349500151111.mp4',
-      title: 'Video Tranh Tường 2',
-      description: 'Mô tả ngắn về video 2',
-      thumbnail: '../../../assets/hinh/z6348903759292_0bb558a2e90c6e890c386205f7eb94c8.jpg'
-    },
-    {
-      id:   '../../../assets/video/6349497873634.mp4',
-      title: 'Video Tranh Tường 3',
-      description: 'Mô tả ngắn về video 3',
-     thumbnail: '../../../assets/hinh/z6348903755964_369d5de2822a8cf705bc8d19e2152080.jpg'
-    },
-    {
-      id:   '../../../assets/video/6349500494127.mp4',
-      title: 'Video Tranh Tường 4',
-      description: 'Mô tả ngắn về video 3',
-      thumbnail: '../../../assets/hinh/z6348903773099_9d11a98017d61cb8093866432dec88b9.jpg'
-    },
-    {
-      id:   '../../../assets/video/6349500847689.mp4',
-      title: 'Video Tranh Tường 5',
-      description: 'Mô tả ngắn về video 3',
-      thumbnail: '../../../assets/hinh/z6348903773099_9d11a98017d61cb8093866432dec88b9.jpg'
-    },
-    {
-      id:   '../../../assets/video/6349501209736.mp4',
-      title: 'Video Tranh Tường 6',
-      description: 'Mô tả ngắn về video 3',
-       thumbnail: '../../../assets/hinh/z6348903818076_cf9f80792855c0dfc41c57978ba5f487.jpg'
-    }
+export class VideoComponent {
+  videos = [
+    { id: 'dQw4w9WgXcQ' },
+    { id: '3JZ_D3ELwOQ' },
+    { id: '2Vv-BfVoq4g' },
+    { id: 'L_jWHffIx5E' },
+    { id: 'kJQP7kiw5Fk' }
   ];
+  isVideoLoaded = false;
 
-  loadedVideos: { [key: string]: boolean } = {};
+  trustedUrl: SafeResourceUrl;
 
-  constructor() { }
-
-  ngOnInit(): void {
-    // Khởi tạo tất cả video là chưa load
-    this.videos.forEach(video => {
-      this.loadedVideos[video.id] = false;
-    });
+  constructor(private sanitizer: DomSanitizer) {
+    this.trustedUrl = this.getSafeUrl(this.videos[0].id);
   }
 
-  loadVideo(videoId: string): void {
-    this.loadedVideos[videoId] = true;
+  playVideo(videoId: string) {
+    this.trustedUrl = this.getSafeUrl(videoId);
+    this.isVideoLoaded = false;
   }
 
-  getVideoUrl(videoId: string): string {
-    return videoId;
-   //return `https://www.youtube.com/embed/${videoId}?autoplay=1`;
+  private getSafeUrl(videoId: string): SafeResourceUrl {
+    this.isVideoLoaded = true;
+    return this.sanitizer.bypassSecurityTrustResourceUrl(`https://www.youtube.com/embed/${videoId}`);
   }
 }
