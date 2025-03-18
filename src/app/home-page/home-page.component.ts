3c5cdd3ca21f77f0088ea9f7238515cfe0bfa195
Webixx-Store <contact.webixx@gmail.com>
2025-03-18 09:47:57 +0700

    A

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -80,25 +80,29 @@ export class HomePageComponent implements OnInit ,  AfterViewInit {
     setTimeout(() => {
       initSlider() ,
 
-    this._swiperService.createSwiper('feedback-swiper', {
-      slidesPerView: 1, // Mặc định 1 feedback
-      spaceBetween: 10,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      grabCursor: true,
-      allowTouchMove: true,
-      breakpoints: {
-        480: { slidesPerView: 1, spaceBetween: 10 }, // Mobile nhỏ
-        768: { slidesPerView: 2, spaceBetween: 15 }, // Tablet
-        1024: { slidesPerView: 3, spaceBetween: 20 }, // Laptop
-      },
-    });
+      this._swiperService.createSwiper('feedback-swiper', {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+        grabCursor: true,
+        allowTouchMove: true,
+        centeredSlides: false, // Không căn giữa để tránh bị lệch
+        breakpoints: {
+          320: { slidesPerView: 1, spaceBetween: 5 }, // Mobile nhỏ
+          480: { slidesPerView: 1, spaceBetween: 10 }, // Điện thoại vừa
+          768: { slidesPerView: 2, spaceBetween: 15 }, // Tablet
+          1024: { slidesPerView: 3, spaceBetween: 20 }, // Laptop
+        },
+      });
+
+
       this._swiperService.createSwiper('reviewSwiperAB', this.swiperConfig);
     }, 500);
     this.loadProduct();
